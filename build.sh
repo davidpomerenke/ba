3e38c7ec19d81b8962f910fad29edeebc9fd115f
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-12-09 20:13:35 +0000

    configure building

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-pdflatex -output-directory pdf BA.tex
+chars=$(detex thesis.tex | wc -m)
+words=$(detex thesis.tex | wc -w)
+printf "\n\n\\\emph{${chars} characters, ${words} words}" > 8-count.tex
+latex thesis.tex
+bibtex thesis.aux
+latex thesis.tex
+latex thesis.tex
